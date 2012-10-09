fd675a05f5e6c7c37c71d755ac00b2982a91066c
xdbaqiao <yexj061761@gamil.com>
2012-10-09 14:34:14 +0800

    pthread

--- pthread.c
+++ pthread.c
@@ -41,7 +41,7 @@ int main(int argc,char *argv[])
 {
     pthread_t pthd;
     pthread_mutex_init(&mut,NULL);
-    pthread_create(&pthd, NULL,(void *)read_function, NULL);
+    pthread_create(&pthd, NULL,(void *)&read_function, NULL);
     write_function();
     return 0;
 }
